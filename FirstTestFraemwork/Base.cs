640e97de50efddf62c477aecabc0976959a55122
Arturlehnytsky <arturlehnytsky@gmail.com>
2019-10-23 13:07:10 +0300

    Lesson 13

---
+++ FirstTestFraemwork/Base.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace FirstTestFraemwork
+{
+    [TestFixture]
+    public class Base
+    {
+        public IWebDriver driver;
+
+
+        [OneTimeSetUp]
+        public void  Init()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [OneTimeSetUp]
+        public void TearDown()
+        {
+
+        }
+
+    }
+}
---
+++ FirstTestFraemwork/NUnitParallelTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+
+namespace FirstTestFraemwork
+{
+    [TestFixture]
+    [Parallelizable]
+    public class NUnitParallelTest : Base
+    {
+        [Test]
+        public void SearchGoogle()
+        {
+           
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.FindElement(By.Name("q")).SendKeys("C#");
+            System.Threading.Thread.Sleep(1000);
+            driver.FindElement(By.Name("btnK")).Click();
+            System.Threading.Thread.Sleep(5000);
+        }
+
+        [Test]
+        public void SearchGoogle1A()
+        {
+
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.FindElement(By.Name("q")).SendKeys("Java");
+            System.Threading.Thread.Sleep(1000);
+            driver.FindElement(By.Name("btnK")).Click();
+            System.Threading.Thread.Sleep(5000);
+        }
+    }
+
+    [TestFixture]
+    [Parallelizable]
+    public class NUnitParallelTest2 : Base
+    {
+        [Test]
+        public void SearchGoogle2()
+        {
+            
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.FindElement(By.Name("q")).SendKeys("C#");
+            System.Threading.Thread.Sleep(1000);
+            driver.FindElement(By.Name("btnK")).Click();
+            System.Threading.Thread.Sleep(10000);
+        }
+    }
+}
